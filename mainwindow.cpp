ad57783517f57a99b374333d37145b704ea69afa
xuxiangyi <Evinxu@users.noreply.github.com>
2018-09-24 17:51:31 +0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,15 +18,20 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    this->setWindowFlags(Qt::FramelessWindowHint);
+    QQweb = new SWebEngineView(this);
+    QUrl url = QUrl("https://w.qq.com/");
+    QQweb->load(url);
+    QQweb->addItemUrl(url);
+    QQweb->show();
 
-    this->setAttribute(Qt::WA_TranslucentBackground, true);
+    //    this->setWindowFlags(Qt::FramelessWindowHint);
+    //    this->setAttribute(Qt::WA_TranslucentBackground, true);
 
+    this->setMaximumSize(1040,642.72);
+    this->setMinimumSize(1040,642.72);
     QDesktopWidget *deskdop = QApplication::desktop();
     move((deskdop->width() - this->width())/2, (deskdop->height() - this->height())/2);
 
-    QObject::connect(ui->openQQbutten,SIGNAL(clicked(bool)),this,SLOT(OpenQQwebButtonSlot()));
-    QObject::connect(ui->QuitButton,SIGNAL(clicked(bool)),this,SLOT(quitButtonSlot()));
 }
 
 MainWindow::~MainWindow()
@@ -34,22 +39,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//打开QQ按钮
-void MainWindow::OpenQQwebButtonSlot()
+void MainWindow::resizeEvent(QResizeEvent *)
 {
-    QDesktopWidget *deskdop = QApplication::desktop();
-    QWebEngineView * QQweb = new QWebEngineView;
-
-    QQweb->load(QUrl("https://w.qq.com/"));
-    QQweb->show();
-    QQweb->setMaximumSize(1040,642.72);
-    QQweb->setMinimumSize(1040,642.72);
-    QQweb->move((deskdop->width() - this->width())/2,(deskdop->height() - this->height())/2);
-
-    this->hide();
-}
-
-void MainWindow::quitButtonSlot()
-{
-    exit(0);
+    QQweb->resize(this->size());
 }
